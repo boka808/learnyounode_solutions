e9a464397b2e1475a1cbecd8441b53797c9dbcc8
slickVM <boka@hawaii.edu>
2014-01-14 15:31:54 -1000

    seventh exercise: http client
    conventional solution

--- httpclient/httpclient.js
+++ httpclient/httpclient.js
@@ -3,15 +3,10 @@ var url = process.argv[2];
 
 http.get(url, function(response) {
 	response.setEncoding('utf8');
-	response.on("data", function(data) {
-		console.log(data);
-	});
+	response.on("data", console.log);
 	response.on("end", function(end) {
 		// do nothing
 		//console.log(end);
 	});
-	response.on("error", function(error) {
-		// do nothing
-		console.error;
-	});
+	response.on("error", console.error);
 });

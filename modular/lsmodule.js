625745234e9cd994c05966b7bcd3942e91212de8
slickVM <boka@hawaii.edu>
2014-01-14 14:44:27 -1000

    sixth assignment: make it modular
    conventional solution

--- modular/lsmodule.js
+++ modular/lsmodule.js
@@ -4,6 +4,13 @@ var pt=require('path');
 module.exports = function (path, ext, output_list) {
 	fs.readdir(path, function (err, ls) {
 		if (err) return output_list(err);
+		// conventional solution
+		ls = ls.filter(function(entry) {
+			return pt.extname(entry) === '.' + ext;
+		});
+		output_list(null, ls);
+
+		/* my solution
 		var list = new Array;
 		ls.forEach(function (entry) {
 			if (pt.extname(entry).slice(1) == ext) {
@@ -11,5 +18,6 @@ module.exports = function (path, ext, output_list) {
 			}
 		});
 		output_list(null, list);
+		*/
 	});
 }
